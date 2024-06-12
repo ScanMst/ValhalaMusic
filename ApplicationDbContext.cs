453fe534354782c5e9e91981322c61eecae77b95
ScanMst <ur226278@gmail.com>
2024-06-12 02:57:11 -0600

    first commit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Valhalla_Music
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        
+    }
+}
