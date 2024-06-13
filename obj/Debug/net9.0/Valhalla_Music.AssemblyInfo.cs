9001d340cfb4285bb4331b2fc58d524cdf222349
Elias Alvarez <al44287@un.edu.mx>
2024-06-13 01:21:23 -0600

    9
    9

--- obj/Debug/net9.0/Valhalla_Music.AssemblyInfo.cs
+++ obj/Debug/net9.0/Valhalla_Music.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Valhalla_Music")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22be3c2d7aa59cd623077cd4aead22c9d3b38ee8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f47d412daad251757fb9c7854a895cad36cc23d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Valhalla_Music")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Valhalla_Music")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
